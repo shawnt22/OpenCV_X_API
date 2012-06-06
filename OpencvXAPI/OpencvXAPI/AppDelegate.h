243fd79702d672212cf60177adca9c1113ca83cc
tengsong <shawnt22@gmail.com>
2012-06-06 15:48:20 +0800

    添加OpenCV X API 基本工程

---
+++ OpencvXAPI/OpencvXAPI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpencvXAPI
+//
+//  Created by 松 滕 on 12-6-5.
+//  Copyright (c) 2012年 Sohu.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpencvXAPI/OpencvXAPI/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OpencvXAPI
+//
+//  Created by 松 滕 on 12-6-5.
+//  Copyright (c) 2012年 Sohu.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
